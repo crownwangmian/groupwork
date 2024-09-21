4cb976daa9baacc4b81c8dc7081763fe3f1b77b9
Bella <wangmian618@gmail.com>
2024-09-21 15:42:49 +0200

    version 2

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("we are here");
+        System.out.println("mian");
+        System.out.println("fjksdjiflkisdjf");
     }
 
 }
